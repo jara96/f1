95477aae4cbcee92e49bf3061ec1c5ea38657fb4
jara96 <fvjara96@gmail.com>
2023-10-16 21:09:26 -0300

    primeros cambios

---
+++ f1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <button className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+            inicio</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
